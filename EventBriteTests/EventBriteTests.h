72573189928a6aed8dd44b08a73c5f98d87088eb
Sujit Anand <sujit_anand@hotmail.com>
2013-04-18 21:15:44 -0700

    Initial Commit

---
+++ EventBriteTests/EventBriteTests.h
@@ -0,0 +1,13 @@
+//
+//  EventBriteTests.h
+//  EventBriteTests
+//
+//  Created by Sujit Anand on 4/18/13.
+//  Copyright (c) 2013 Sujit Anand. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EventBriteTests : SenTestCase
+
+@end
---
+++ EventBriteTests/EventBriteTests.m
@@ -0,0 +1,32 @@
+//
+//  EventBriteTests.m
+//  EventBriteTests
+//
+//  Created by Sujit Anand on 4/18/13.
+//  Copyright (c) 2013 Sujit Anand. All rights reserved.
+//
+
+#import "EventBriteTests.h"
+
+@implementation EventBriteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EventBriteTests");
+}
+
+@end
